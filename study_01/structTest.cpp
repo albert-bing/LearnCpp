f7a6fcd51bb568d20fc52dc0826d1b1d23e43a5f
liuxiaobing03 <liuxiaobing03@baidu.com>
2023-09-23 15:45:58 +0800

    "change struct1"

---
+++ study_01/structTest.cpp
@@ -0,0 +1,20 @@
+//
+// Created by liuxiaobing03 on 2023/9/12.
+//
+
+#include "structTest.h"
+#include<iostream>
+
+using namespace std;
+
+// 定义结构体
+struct Student{
+    string name;
+    int age;
+    int score;
+};
+
+int main(){
+
+
+}
---
+++ study_01/structTest.h
@@ -0,0 +1,14 @@
+//
+// Created by liuxiaobing03 on 2023/9/12.
+//
+
+#ifndef LEARN_STRUCTTEST_H
+#define LEARN_STRUCTTEST_H
+
+
+class structTest {
+
+};
+
+
+#endif //LEARN_STRUCTTEST_H
